00e35136b37d552e620d91b7193cf4cfb001b3c0
Oliver Hung Buo Tso <olivertsor@gmail.com>
2018-03-25 22:43:03 -0300

    1.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pdepy',
-    version='1.0.0',
+    version='1.0.1',
     description='A Finite-Difference PDE solver.',
     long_description=long_description,
     url='https://github.com/olivertso/pdepy',
